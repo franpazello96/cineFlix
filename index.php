5cb126084bc5ff55c59f023422ef38f9cc58f533
Francielly Pazello <franciellypz_96@hotmail.com>
2023-03-26 10:46:22 -0300

    Criadas as páginas iniciais do projeto

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <?php require_once 'link.php';?>
+    <?php require_once 'menu.php';?>
+    <body>
+        <!--Inicio Principal-->
+        <section id="home" class="d-flex">
+            <div class="container align-self-center">
+                <div class="row">
+                    <div class="col-md-12 capa">
+                        <h1 >Filmes, séries e muito mais. Sem limites.</h1>
+                        <h6 >Assista onde quiser. </h6>
+                        <a href="login-cadastro.php" class="btn  btn-azul btn-lg btn-custom">Criar Assinatura.</a>
+                        <a href="parceiros.php" class="btn  btn-branco btn-lg btn-custom">NOSSOS FILMES </a>		
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Fim Principal-->
+        <!--Inicio Conteudo-->
+        <section id="conteudo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem1.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem2.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem3.png" width="350" height="350" class="img-fluid d-none d-md-block" >
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem4.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem1.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem2.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem3.png" width="350" height="350" class="img-fluid d-none d-md-block" >
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem4.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem1.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem2.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem3.png" width="350" height="350" class="img-fluid d-none d-md-block" >
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem4.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6">
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem1.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem2.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                      <div class="row albuns">
+                        <div class="col-md-6">
+                            <img src="imagens/imagem3.png" width="350" height="350" class="img-fluid d-none d-md-block" >
+                        </div>
+                        <div class="col-md-6">
+                            <img src="imagens/imagem4.png" width="350" height="350" class="img-fluid d-none d-md-block">
+                        </div>
+                      </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+<?php require_once 'rodape.php'?>
+<!---- JavaScript ----->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php require_once 'link.php';?>
+    <?php require_once 'menu.php';?>
+    
+</head>
+<body>
+    <section id="login" >
+        <div class="container aling-self-center"> 
+            <div class="row">
+                <div class="col-md-12">
+                <img src="imagens/perfil.png"  witdh="15%" height="15%" class="imagem mt-5"> 
+                         <form action="login_valida.php" method="POST" name="login_valida" id="login_valida">
+                            <div class="form-group m-3">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="E-mail:" required>
+                            </div>
+                            <div class="form-group m-3">
+                                <input type="password" class="form-control mb-2" name="senha" id="senha" placeholder="Senha:" required>
+                                <p >Não tem uma conta? <a href="login-cadastro.php">Cadastre-se</a><p>
+                                <p ><a href="" style="font-size: 12px;">Esqueceu sua senha?</a><p>
+                            </div>
+                            <div>
+                            <a href="index.php" class="btn btn-secondary">Voltar</a>	
+                            <button type="submit" class="btn btn-primary " >Entrar</button>
+                            </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
