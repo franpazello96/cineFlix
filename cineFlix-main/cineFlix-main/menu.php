2a5d0f77ecda183c63cbdd218fd8c32279772837
Junior <jrinho22@gmail.com>
2023-04-05 20:12:00 -0300

    feat: login com firebase

--- cineFlix-main/cineFlix-main/menu.php
+++ cineFlix-main/cineFlix-main/menu.php
@@ -4,6 +4,7 @@
     <?php require_once 'link.php';?>
     </head>
     <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <header>
             <!---Inicio Navbar--->
             <nav class="navbar navbar-expand-md navbar-light fixed-top navbar-transparente">
@@ -32,11 +33,26 @@
                             <li  class="nav-item divisor"></li>
                             
                            <!-- <li  class="nav-item">
+                           <li  class="nav-item">
                                 <a href="login-cadastro.php" class="nav-link">INSCREVER-SE</a>
-                            </li>-->
-                            <li  class="nav-item">
+                            </li>
+                            <li  id='entrar' class="nav-item" style="display: flex; align-items: center;">
                                 <a href="login.php" class="nav-link">ENTRAR</a>
                             </li>
+                            <!-- <div class="dropdown">
+                                <button onclick="myFunction()" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    SEARCH
+                                </button>
+                            <div id="search" style="background-color: darkred">
+                                <input id='movieTitle' style="border:0; outline:0" placeholder="Pesquise por título"/>
+                                <select style="margin-left: 10px" aria-label="Pesquise por genero">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                            </div> -->
                         </ul>
                     </div>
                 </div>
@@ -44,3 +60,40 @@
         </header> 
     </body>
 </html>
+
+<script>
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const logged = urlParams.get('logged');
+console.log(logged, 'ghjbv');
+if ( logged ) {
+    document.getElementById("entrar").innerHTML = '<a class="nav-link">LOGADO</a><img src="imagens/perfil.png"  style="background-color:black;" witdh="5px" height="25px" class="imagem">';
+}
+
+function myFunction() {
+  var search = document.getElementById("search");
+  if (search.style.display === "none") {
+    search.style.display = "flex";
+  } else {
+    search.style.display = "none";
+  }
+
+}
+
+document.addEventListener("keyup", event => {
+    if (event.keyCode === 13) {
+        var movieTitle = document.getElementById("movieTitle");
+        alert(movieTitle.value)
+    }
+});
+</script> 
+
+<style>
+    #search {
+            position: absolute;
+            display: none;
+            right: 0px;
+            top: 42px;
+            padding: 10px
+        }
+</style>
---
+++ cineFlix-main/cineFlix-main/script/firebase/firebaseApp.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyA7T48tHf4P7IIiAtSo6ZVJHeY5JRky3lg",
+authDomain: "experiencia-criativa-f8b3c.firebaseapp.com",
+projectId: "experiencia-criativa-f8b3c",
+storageBucket: "experiencia-criativa-f8b3c.appspot.com",
+messagingSenderId: "545388503575",
+appId: "1:545388503575:web:b3157166cb32cf3c37f252"
+};
+const app = initializeApp(firebaseConfig);
+
+export default app
