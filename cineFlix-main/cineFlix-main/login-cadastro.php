d9963b84fae5e5d71d539321e75df8796706765b
Micael Daniel Pardim Fone <104154840+MicaelFone@users.noreply.github.com>
2023-04-05 18:24:55 -0300

    Update login-cadastro.php
    Atualização da página de cadastro do usuário, com a finalização da implementação da validação da data de nascimento ,finalização da implementação da validação de dados do CEP ,CPF, Nome, Data Nascimento com o uso de REGex.

--- cineFlix-main/cineFlix-main/login-cadastro.php
+++ cineFlix-main/cineFlix-main/login-cadastro.php
@@ -6,9 +6,33 @@
     <link rel="stylesheet" href="css/layout.css" type="text/css">
 </head>
 <body>
+    <script>
+        function validardataDeNascimento(data){
+            dataAtual= new Date();
+
+            data=new Date(data);
+            	if(data<=1900){
+                window.alert("Data inválida!");
+                return false; 
+            } 
+            
+            if (data<dataAtual){
+                console.log("Data Válida");
+                return true;
+            } else {
+                window.alert("Data inválida!");
+
+                return false;
+            }
+        }
+    </script>   
+    <style>
+        body {
+            background-color: #d4390ad5;
+        }
+    </style>       
     <script type="text/javascript" src="script/script.js"></script>
 	<script type="text/javascript" src="script/CPF.js"></script>
-	<script type="text/javascript" src="script/data.js"></script>
     <script type="text/javascript" src="script/enderenco.js"></script>
     <div class="content">
         <form method="GET" action="form_action.html">
@@ -22,47 +46,47 @@
                     <td><input type=text name="Nome" pattern="[A-Za-záàâãéèêíóôõúç\s]{8,50}" placeholder="Insira um nome válido" title="Nome com 8 a 50 letras" required>
                 </tr><tr>
 			    
-				</tr><tr>
+		</tr><tr>
                     <td>CPF:</td>
-                    <td><input type="text" name="cpf" id="cpf"
-                    minlength="14" maxlength="14" placeholder="CPF com apenas números"
+                    <td><input type="text" name="CPF" id="cpf" title="Deve contar 8 dígitos númericos sem o uso de caracteres especiais"
+                    minlength="14" maxlength="14" placeholder="Insira um CEP válido" 
                       onkeyup="mask(this)" ></td> 
                 </tr><tr>
                     <td>Data Nascimento:</td>
-                    <td><input type=date id="Datanasc" pattern="[0-9]{2}/[0-9]{2}/[0-9]{2}" onblur="data_valida(date)" title="Data Nascimento" required></td>
+                    <td> <input name="Data de nascimento" onchange="validardataDeNascimento(this.value);" pattern="([0-9]{2})/([0-9]{2})/([0-9]{4})" id="dataDeNascimento" type="date" placeholder="Insira uma data válida" title="Deve seguir o formato seguinte data/mes/ano" required>
                 </tr><tr>
                 
 				<td>Email:</td>
-                    <td><input type=text name="EMAIL" pattern="^[A-Za-z0-9_.-]+@([A-Za-z0-9_]+\.)+[A-Za-z]{2,4}$" placeholder="nome do email@email.com"  title="Telefone" required>
+                    <td><input type=text name="Email" pattern="^[A-Za-z0-9_.-]+@([A-Za-z0-9_]+\.)+[A-Za-z]{2,4}$" placeholder="Insira um email válido"  title="Deve coonter o nome do email seguido com o @.Ex:nomeemail@email.com" required>
                
 				</tr><tr>
 					<td>Telefone:</td>
-					<td><input type="phone" name="celular" id="celular" pattern="\(\d{2}\)\s\d{4,5}-\d{4}$"                    
-                    onkeydown="return mascaraTelefone(event)" placeholder="(DD)9xxxx-xxxx" title="(xx) xxxxx-xxxx" required>
+					<td><input type="phone" name="Celular" id="celular" pattern="\(\d{2}\)\s\d{4,5}-\d{4}$"                    
+                    onkeydown="return mascaraTelefone(event)"  placeholder="Insira um telefone válido" title=" Deve conter 11 digitos sem caracteres especiais,os dois primeiros dígitos são o DD e o primeiro dígito do telefone é sempre 9. ex:(xx) xxxxx-xxxx" required>
 					</td>
 				</tr><tr>
                     <td>Senha:</td>
-                    <td><input type=password pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&*]).{6,10}" name="Senha" placeholder="Crie uma senha"  title="Deve conter pelo menos um número e uma letra maiúscula e minúscula e pelo menos 6 ou mais caracteres." required>
+                    <td><input type=password pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&*]).{6,10}" name="Senha" placeholder="Insira uma senha válida"  title="Deve conter pelo menos um número e uma letra maiúscula e minúscula e pelo menos 6 ou mais caracteres." required>
                     </td>
                 </tr><tr>
                     <td>CEP:</td>
-                      <td><input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
-               onblur= "pesquisacep(this.value);" required></td>
+                      <td><input name="CEP" type="text" id="cep" value="" size="10" maxlength="9"
+               onblur= "pesquisacep(this.value);"placeholder="Insira um CEP válido" title="Deve conter nove dígitos 8 númericos.O uso de hifen é opcional.Ex de CEP válido:xxxxx-xxx ou xxxxxxxx" required></td>
                </tr><tr> 
                 <td>RUA:</td>
-                <td><input name="rua" type="text" id="rua" size="60" /></td>    
+                <td><input name="Rua" type="text" id="rua" size="60" /></td>    
                 </tr><tr>
                 </tr><tr> 
                 <td>Estado:</td>
-                <td><input name="uf" type="text" id="uf" size="2" /></td>    
+                <td><input name="Uf" type="text" id="uf" size="2" /></td>    
                 </tr><tr>
                 </tr><tr> 
                     <td>Cidade:</td>
-                <td><input name="cidade" type="text" id="cidade" size="40" /></td>    
+                <td><input name="Cidade" type="text" id="cidade" size="40" /></td>    
                 </tr><tr>
                 <th colspan="2">
-                    <input type=submit>
-                    <input type=reset value="Limpar Campos">
+                    <input type="submit">
+                    <input type="reset" value="Limpar Campos">
                 </th>
                 </tr>
             </table>
