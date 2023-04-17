cb7236a650bf7a6286f9fdd7c71dfcc19f8d31a0
Junior <jrinho22@gmail.com>
2023-04-17 20:00:17 -0300

    Deixando cadastro dinamico conforme tipo de pessoa

--- login-cadastro.php
+++ login-cadastro.php
@@ -17,17 +17,29 @@
                             <div class="form-group">
                                 <select class="form-control  m-2 col-12" id="tipo_pessoa" name="tipo_pessoa" onchange="exibir_ocultar(this)">
                                     <option id="selecione" value="selecione" name="selecione">Selecione</option>
-                                    <option id="pessoa_fisica" value="pessoa_fisica" name="pessoa_fisica">Pessoa Fisica</option>
+                                    <option selected id="pessoa_fisica" value="pessoa_fisica" name="pessoa_fisica">Pessoa Fisica</option>
                                     <option id="pessoa_juridica" value="pessoa_juridica" name="pessoa_juridica">Pessoa Juridica</option>
                                 </select>
-                                    <input class="form-control m-2 col-12" type="text" id="nome" name="nome" placeholder="Nome: " >
+                                <div id="pessoa_juridica_campos">
                                     <input class="form-control m-2 col-12" type="text" id="razao" name="razao" placeholder="Razão Social: " >
                                     <input class="form-control m-2 col-12" type="text" id="fantasia" name="fantasia" placeholder="Nome Fantasia: " >
+                                    <input class="form-control m-2 col-12" type="text" id="cnpj" name="cnpj" placeholder="CNPJ: " >
                                     <input class="form-control m-2 col-12" type="email"id="email" name="email" placeholder="E-mail:  " >
                                     <input class="form-control m-2 col-12" type="password"id="senha" name="senha" placeholder="Senha:  " >
+                                    <input class="form-control m-2 col-12" type="text" id="logradouro" name="logradouro" placeholder="Logradouro: " >
+                                    <input class="form-control m-2 col-12" type="text" id="numero" name="numero" placeholder="Número: " >
+                                    <input class="form-control m-2 col-12" type="text" id="complemento" name="complemento" placeholder="Complemento: " >
+                                    <input class="form-control m-2 col-12" type="text" id="bairro" name="bairro" placeholder="Bairro: " >
+                                    <input class="form-control m-2 col-12" type="text" id="cidade" name="cidade" placeholder="Cidade: " >
+                                    <input class="form-control m-2 col-12" type="text" id="uf" name="uf" placeholder="Estado: " >
+                                    <input class="form-control m-2 col-12" type="text" id="telefone" name="telefone" placeholder="Telefone: ">
+                                </div>
+                                <div id="pessoa_fisica_campos">
+                                    <input class="form-control m-2 col-12" type="text" id="nome" name="nome" placeholder="Nome: " >
                                     <input class="form-control m-2 col-12" type="text" id="cpf" name="cpf" placeholder="CPF: " >
+                                    <input class="form-control m-2 col-12" type="email"id="email" name="email" placeholder="E-mail:  " >
+                                    <input class="form-control m-2 col-12" type="password"id="senha" name="senha" placeholder="Senha:  " >
                                     <input class="form-control m-2 col-12" type="text" id="dataDeNascimento" name="dataDeNascimento" placeholder="Data de Nascimento: " >
-                                    <input class="form-control m-2 col-12" type="text" id="cnpj" name="cnpj" placeholder="CNPJ: " >
                                     <input class="form-control m-2 col-12" type="text" id="cep" name="cep" placeholder="CEP: " >
                                     <input class="form-control m-2 col-12" type="text" id="logradouro" name="logradouro" placeholder="Logradouro: " >
                                     <input class="form-control m-2 col-12" type="text" id="numero" name="numero" placeholder="Número: " >
@@ -36,6 +48,7 @@
                                     <input class="form-control m-2 col-12" type="text" id="cidade" name="cidade" placeholder="Cidade: " >
                                     <input class="form-control m-2 col-12" type="text" id="uf" name="uf" placeholder="Estado: " >
                                     <input class="form-control m-2 col-12" type="text" id="telefone" name="telefone" placeholder="Telefone: ">
+                                </div>       
                                     <a href="index.php" class="btn btn-secondary ml-2">Voltar</a>	
                                     <button  type="submit" class="btn btn-primary ">Cadastrar</button>
                             </div>
@@ -53,4 +66,24 @@
     </section>
     <?php require_once 'rodape.php'?>
 </body>
+<script>
+    const pessoa_fisica_campos = document.getElementById("pessoa_fisica_campos")
+    const pessoa_juridica_campos = document.getElementById("pessoa_juridica_campos")
+    pessoa_juridica_campos.classList.add("hidden");
+    function exibir_ocultar(e) {
+        if (e.value == 'pessoa_fisica') {
+            pessoa_juridica_campos.classList.add("hidden");
+            pessoa_fisica_campos.classList.remove("hidden");
+        } else {
+            pessoa_fisica_campos.classList.add("hidden");
+            pessoa_juridica_campos.classList.remove("hidden");
+        }
+    }
+
+</script>
+<style>
+    .hidden {
+        display: none;
+}
+</style>
 </html>
