22c04413077f8f5a437dbfdc3c018311267ccd03
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-04-25 22:24:12 -0300

    adicionando filme no banco

---
+++ adiciona-filme-grava.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'conexao.php';
+
+$titulo = $_POST['titulo'];
+$descricao = $_POST['descricao'];
+$genero = $_POST['genero'];
+$diretor = $_POST['diretor'];
+$ano = $_POST['ano'];
+$duracao = $_POST['duracao'];
+
+$sql = "INSERT INTO filme (titulo, descricao, genero, diretor, ano, duracao)
+VALUES ('$titulo','$descricao', '$genero', '$diretor', 0, '$duracao')";
+
+$resultado = mysqli_query($conn,$sql);
+
+if($resultado){
+    header('Location:index.php');
+} else {
+    echo "Erro na execução da consulta: " . mysqli_error($conn);
+}
+?>
--- adiciona-filme.php
+++ adiciona-filme.php
@@ -1,9 +1,3 @@
-<?php
-//require_once 'conexao.php';
-//session_start();
-
-$id = $_SESSION['id'];
-?> 
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -13,54 +7,34 @@ $id = $_SESSION['id'];
     <link rel="stylesheet" href="css/layout.css" type="text/css">
 </head>
 
-<?php
-    if (isset($_SESSION['id']))
-    {
-        $id = $_SESSION['id']; 
-        $sql = "SELECT * FROM usuario WHERE id = '$id' "; 
-        $resultado = $conn->query($sql);
-        if($resultado->num_rows > 0){
-            while($registro = $resultado->fetch_assoc()){  
-    ?>
-                <h5 style="font-size: 1em;" class="mt-4 text-left">Usuario: <?=$registro ["email"] ?></h5>
-    <?php
-        }
-    }
-}
-?>
-
 <body>
     <div class="content">
         <div class="container aling-self-center">
         <div style="padding-top: 15%;">
-        <form method="POST" action="login_grava.php" name="login_grava" id="login_grava" class="col-md-12" style="background-color: #dbdbdb; border-radius: 30px;">
+        <form method="POST" action="adiciona-filme-grava.php" name="adiciona-filme-grava" id="adiciona-filme-grava" class="col-md-12" style="background-color: #dbdbdb; border-radius: 30px;">
             <div class="form-group m-3" style="padding-top: 5%;">
                 <label for="titulo">Título</label>
-                <input type="text" class="form-control" id="titulo" placeholder="Título">
+                <input type="text" id="titulo" class="form-control"  placeholder="Título">
             </div>
             <div class="form-group m-3">
                 <label for="descricao">Descrição</label>
-                <input type="text" class="form-control" id="descricao" placeholder="Descrição">
+                <input type="text" id="descricao" class="form-control"  placeholder="Descrição">
             </div>
             <div class="form-group m-3">
                 <label for="genero">Gênero</label>
-                <input type="text" class="form-control" id="genero" placeholder="Gênero">
+                <input type="text" id="genero" class="form-control"  placeholder="Gênero">
             </div>
             <div class="form-group m-3">
                 <label for="diretor">Diretor</label>
-                <input type="text" class="form-control" id="diretor" placeholder="Diretor">
-            </div>
-            <div class="form-group m-3">
-                <label for="tipo">Tipo (Filme, Serie, etc...)</label>
-                <input type="text" class="form-control" id="tipo" placeholder="Tipo">
+                <input type="text" id="diretor" class="form-control"  placeholder="Diretor">
             </div>
             <div class="form-group m-3">
                 <label for="ano">Ano</label>
-                <input type="number" class="form-control" id="ano" placeholder="Ano">
+                <input type="number" id="ano" class="form-control"  placeholder="Ano">
             </div>
             <div class="form-group m-3">
                 <label for="duracao">Duração (Horas)</label>
-                <input type="number" class="form-control" id="duracao" placeholder="Duração">
+                <input type="number" id="duracao" class="form-control"  placeholder="Duração">
             </div>
             <div style="padding-bottom: 5%; padding-left: 40%">
                 <a href="index.php" class="btn btn-secondary">Cancelar</a>	
@@ -70,5 +44,6 @@ $id = $_SESSION['id'];
         </div>
         </div>
     </div>
+    <?php require_once 'rodape.php'?>
 </body>
 </html>
