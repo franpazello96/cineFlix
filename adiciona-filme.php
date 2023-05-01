25a5da66dfba71c30a708c50d429c2148f0ec302
Micael Daniel Pardim Fone <104154840+MicaelFone@users.noreply.github.com>
2023-05-01 19:58:53 -0300

    Update adiciona-filme.php
    Implementando validações de cadastro dos filmes

--- adiciona-filme.php
+++ adiciona-filme.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <?php require_once 'link.php'; ?>
-    <?php require_once 'menu.php'; ?>
-    <link rel="stylesheet" href="css/layout.css" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
@@ -21,15 +12,15 @@
                 <form method="POST" action="adiciona-filme-grava.php" name="adiciona-filme-grava" id="adiciona-filme-grava" class="col-md-12" style="background-color: #dbdbdb; border-radius: 30px;">
                     <div class="form-group m-3" style="padding-top: 5%;">
                         <label for="titulo">Título</label>
-                        <input type="text" id="titulo" class="form-control" name="titulo" placeholder="Título" required>
+                        <input type="text" id="titulo" class="form-control" name="titulo" placeholder="Título" title="O nome da produção não possui limites de caracteres" required>
                     </div>
                     <div class="form-group m-3">
                         <label for="descricao">Descrição</label>
-                        <input type="text" id="descricao" class="form-control" name="descricao" placeholder="Descrição" required>
+                        <input type="text" id="descricao" class="form-control" name="descricao" placeholder="Descrição" title="A descrição da produção precisa ser inserida de uma forma de sinopse" required>
                     </div>
                     <div class="form-group m-3">
                         <label for="genero">Gênero</label>
-                        <select id="genero" class="form-control" name="genero" required>
+                        <select id="genero" class="form-control" title="Selecione apenas um tipo de genero em que esta produção pertence" name="genero" required>
                             <option value="">Selecione um gênero</option>
                             <option value="Ação">Ação</option>
                             <option value="Animação">Animação</option>
@@ -44,7 +35,7 @@
                     </div>
                     <div class="form-group m-3">
                         <label for="diretor">Diretor</label>
-                        <select id="diretor" class="form-control" name="diretor" required>
+                        <select id="diretor" class="form-control" name="diretor" title="Selecione apenas um diretor da produção" required>
                             <option value="">Selecione um diretor</option>
                             <option value="Christopher Nolan">Christopher Nolan</option>
                             <option value="Steven Spielberg">Steven Spielberg</option>
@@ -59,11 +50,11 @@
                     </div>
                     <div class="form-group m-3">
                         <label for="ano">Ano</label>
-                        <input type="text" id="ano" class="form-control" name="ano" placeholder="Ano" pattern="\d*" maxlength="4" required>
+                        <input type="text" id="ano" class="form-control" name="ano" placeholder="Ano" pattern="\d*" title="O ano é referente a data de lançamento da produção" maxlength="4" required>
                     </div>
                     <div class="form-group m-3">
                         <label for="duracao">Duração em minutos</label>
-                        <input type="number" id="duracao" class="form-control" name="duracao" placeholder="Duração" required>
+                        <input type="number" id="duracao" title="A duracao da producao pode ser inserida tanto em minutos ou tanto em horas." class="form-control" name="duracao" placeholder="Duração" required>
                     </div>
                     <div style="padding-bottom: 5%; padding-left: 40%">
                         <a href="index.php" class="btn btn-secondary">Cancelar</a>
@@ -86,55 +77,35 @@
         }
 
     }
+    function validaDuracao(duracao){
 
+        if (isNaN(duracao) || duracao <= 0) {
+            return false;
+        }
+
+        return true;
+    }
 
 
     // Seleciona o formulário pelo ID
-    const form = document.querySelector('#login_grava');
+    const form = document.querySelector('#adiciona-filme-grava');
 
     // Seleciona os campos de entrada pelo ID
-    const titulo = document.querySelector('#titulo');
-    const descricao = document.querySelector('#descricao');
-    const genero = document.querySelector('#genero');
-    const diretor = document.querySelector('#diretor');
-    const tipo = document.querySelector('#tipo');
-    const ano = document.querySelector('#ano');
-    const duracao = document.querySelector('#duracao');
+    const duration = document.querySelector('#duracao');
 
     // Adiciona um ouvinte de evento de envio ao formulário
     form.addEventListener('submit', (event) => {
         // Verifica se os campos estão vazios ou não preenchidos corretamente
-        if (!titulo.value) {
-            alert('Por favor, preencha o campo Título.');
+       
+        if (!validaDuracao(duration)){
+            alert("Duracão invalida!!!;Por favor insira um duração válida");
             event.preventDefault();
+            document.getElementById("duracao").value=null;
         }
-
-        if (!descricao.value) {
-            alert('Por favor, preencha o campo Descrição.');
-            event.preventDefault();
-        }
-
-        if (!genero.value) {
-            alert('Por favor, preencha o campo Gênero.');
-            event.preventDefault();
-        }
-
-        if (!diretor.value) {
-            alert('Por favor, preencha o campo Diretor.');
-            event.preventDefault();
-        }
-
-        if (!tipo.value) {
-            alert('Por favor, preencha o campo Tipo.');
-            event.preventDefault();
-        }
-
-        if (!duracao.value || duracao.value <= 0) {
-            alert('Por favor, preencha o campo Duração com uma duração válida.');
-            event.preventDefault();
-        }
+    
 
     });
+
 
 
 </script>
